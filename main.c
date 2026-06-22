f4f985c49e5d23c4d826182b32e75baa52d82cb6
mpfrfr <mariapaulasantacruz01@gmail.com>
2026-06-21 22:06:01 -0500

    main.c

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main(void)
 
     printf("\n");
     separador();
-    printf("        BIENVENIDO A WEARCHER\n");
+    printf("                   BIENVENIDO A WEARCHER\n");
     separador();
 
     for (int i = 0; i < NUM_ZONAS; i++)
